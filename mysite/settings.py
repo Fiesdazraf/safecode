d8fba3c193336cdae12979bc50d87f2613d51981
fiesdazraf <8leorave@gmail.com>
2025-07-21 06:49:44 +0330

    email app password

--- mysite/settings.py
+++ mysite/settings.py
@@ -132,7 +132,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'farzad.seif30@gmail.com'  
-EMAIL_HOST_PASSWORD = 'gkpy irwq jyvw xmly'  
+EMAIL_HOST_PASSWORD = 'gkpyirwqjyvwxmly'  
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
