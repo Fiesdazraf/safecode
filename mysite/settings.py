8e85c18c6151317cc3fa56a59bf1e863305c23a7
fiesdazraf <8leorave@gmail.com>
2025-07-21 04:25:27 +0330

    adding csrf tokken for railway

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,8 +26,9 @@ SECRET_KEY = "django-insecure-*#)1$_h#$4*j0dmq1+9o$a7a4qswc2jth9wrx^wk4jk2o^*m)0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['web-production-b388a.up.railway.app']
 
+CSRF_TRUSTED_ORIGINS = ['https://web-production-b388a.up.railway.app']
 
 # Application definition
 
