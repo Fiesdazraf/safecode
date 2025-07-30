7e29ecc310c04b7d411c6fd983b033bfbbe1b1e3
fiesdazraf <8leorave@gmail.com>
2025-07-30 05:49:02 +0330

    deleting repetetive route in urls.py

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home_view, name='home'),
     path('portfolio/', views.portfolio, name='portfolio'),
     path('contact/', views.contact, name='contact'),
     path('test-email/', views.test_email, name='test_email'),
