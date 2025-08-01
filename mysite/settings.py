cff6201022e645b07842695199649f4d906aac10
fiesdazraf <8leorave@gmail.com>
2025-08-01 06:19:53 +0330

    chore: configure EMAIL_BACKEND for console and prepare for SMTP setup

--- mysite/settings.py
+++ mysite/settings.py
@@ -144,7 +144,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'farzad.seif30@gmail.com'  
-EMAIL_HOST_PASSWORD = 'gkpyirwqjyvwxmly'  
+EMAIL_HOST_PASSWORD = 'zsimkqqnqzxtgfry'  
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
