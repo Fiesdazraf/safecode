585376c012a3a703474901b21dc6c998bb96e5dc
fiesdazraf <8leorave@gmail.com>
2025-08-02 05:33:43 +0330

    making placeholders rtl

--- main/forms.py
+++ main/forms.py
@@ -13,17 +13,17 @@ class ContactForm(forms.ModelForm):
         widgets = {
             'name': forms.TextInput(attrs={
                 'placeholder': 'نام شما',
-                'class': 'form-control mb-3 text-end',
-                'dir': 'rtl',
+                'class': 'form-control mb-3 text-end rtl-input',
+                'dir': 'rtl'
             }),
             'email': forms.EmailInput(attrs={
                 'placeholder': 'ایمیل شما',
-                'class': 'form-control mb-3 text-end',
-                'dir': 'rtl',
+                'class': 'form-control mb-3 text-end rtl-input',
+                'dir': 'rtl'
             }),
             'message': forms.Textarea(attrs={
                 'placeholder': 'متن پیام شما',
-                'class': 'form-control mb-3 text-end',
+                'class': 'form-control mb-3 text-end rtl-input',
                 'rows': 5,
                 'dir': 'rtl',
             }),
